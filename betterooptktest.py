178adb54477a829f6523cf803256aeaa3f454c4e
Theallpro1 <38364214+Theallpro1@users.noreply.github.com>
2022-07-11 21:51:22 -0500

    Drastically improved file selector along with a few other minor changes

--- betterooptktest.py
+++ betterooptktest.py
@@ -19,29 +19,16 @@ for (dirpath, dirnames, filenames) in os.walk(sample_dir):
 	break
 
 # TODO make it so that you can chose directories as well.
-# TODO THe dropdown menu does not include the original file
-# When multiple of the same file inserted already dd
 # Averages
-# for widget in self.graph_frame.winfo_children():
-# print(widget)
-# widget.destroy()
-# fix errors about deleting files when there are none left
-# TODO use print ("\a")
-# Fit the Ion Saturation TODO
-# m,b = np.polyfit(x[:fit_top],y[:fit_top],1)
-# plotting the graph
-# plot1.plot(x,m*x+b)	
-# Graph legend
 # Try to update graph without redrawing the whole thing, messing up scales
-# Hold data in a map to not reaccesss it every time you regraph, this might be slowing down the code
-# Box average manipulation
+# multiple labels in the selector frame are made for adding the same thing
 
 class App(ctk.CTk):
 	def __init__(self):
 		super().__init__()
 		
 		# Set up the basic window things	
-		self.WIDTH = 1000
+		self.WIDTH = 1400
 		self.HEIGHT = 5000
 		self.title("Langmuir Experiment Analyzer Program")
 		self.geometry("%ix%i" % (self.WIDTH, self.HEIGHT))
@@ -51,7 +38,11 @@ class App(ctk.CTk):
 
 		# This list holds the filenames of the graphs that are displayed, along with their data
 		self.currently_displayed = {}
+		self.selector_display = {}
 		self.view_mode = 0	
+		self.lin_log = 0
+		self.select_all = tk.IntVar()
+		self.legend_visibility = True
 		self.fit_bound = [tk.IntVar(value=0), tk.IntVar(value=0)]
 	
 		# Frames 
@@ -59,7 +50,8 @@ class App(ctk.CTk):
 		self.control_frame = ctk.CTkFrame()	# Holds the controls for manipulating the graphs.
 		self.adding_frame = ctk.CTkFrame()	# Holds the controls for adding and removing files from the graph.
 		self.cursor_frame = ctk.CTkFrame(master = self.control_frame)	
-		
+		self.selector_frame = ctk.CTkFrame()	
+	
 		# Plots a new graph on the screen	
 		self.plot_button = ctk.CTkButton(master = self.adding_frame,
 			command = self.add_new_graph,
@@ -93,8 +85,24 @@ class App(ctk.CTk):
 			command = lambda: self.delete_file(self.file_selector.get()),
 	                text = "Delete")
 		self.viewmode_button = ctk.CTkButton(master = self.control_frame,
-			command = lambda: self.plot(True),
+			command = self.vm_toggle,
 			text = "Viewmode")
+		self.derivative_button = ctk.CTkButton(master = self.control_frame,
+			command = self.derivative,
+			text = "f'")
+		self.scale_button = ctk.CTkButton(master = self.control_frame,
+			command = self.toggle_graph_scale,
+			text = "lin/log")
+		self.legend_button = ctk.CTkButton(master = self.control_frame,
+			command = self.toggle_legend,
+			text = "legend")
+		self.box_button = ctk.CTkButton(master = self.control_frame,
+			command = self.box_average,
+			text = "box average")
+		self.all_button = ctk.CTkCheckBox(master = self.control_frame,
+			command = self.all,
+			variable = self.select_all,
+			text = "")
 
 		self.fit_counter = ctk.CTkLabel(master = self.control_frame, textvar = self.fit_bound[0])
 		self.file_addition_selector = ctk.CTkOptionMenu(master = self.adding_frame, values=fnames)
@@ -102,12 +110,13 @@ class App(ctk.CTk):
 
 		# Put the widgets on the screen
 		self.redraw_widgets()
-	
+
 	def redraw_widgets(self):
-		self.control_frame.grid(row=0,column=1, padx = 10, pady = 10)
+		self.control_frame.grid(row = 0, column = 1, padx = 10, pady = 10)
 		self.graph_frame.grid(row=0, column = 0)
 		self.adding_frame.grid(row=1, column = 0)
 		self.plot_button.grid(row=0, column=1)
+		self.selector_frame.grid(row = 0, column = 2)
 		self.cursor_frame.pack()
 		self.minus_button_el.grid(row=0,column=0)
 		self.minus_button_l.grid(row=0,column=1)
@@ -120,10 +129,59 @@ class App(ctk.CTk):
 		self.file_selector.pack()
 		self.deletion_button.pack()
 		self.viewmode_button.pack()
+		self.derivative_button.pack()
+		self.scale_button.pack()	
+		self.legend_button.pack()
+		self.box_button.pack()
+		self.all_button.pack()
+
+	def all(self):
+		v = self.select_all.get()
+		for key in self.selector_display:
+			self.selector_display[key][1].set(v)
+
+	def toggle_legend(self):
+		if self.legend_visibility == False:
+			self.legend_visibility = True
+		elif self.legend_visibility == True:
+			self.legend_visibility = False
+		self.plot()
+
+	def toggle_graph_scale(self):
+		if self.lin_log == 0:
+			self.lin_log = 1
+		elif self.lin_log == 1:
+			self.lin_log = 0
+		self.plot()
+
+	def get_selected(self):
+		selected = []
+		for key in self.selector_display:
+			if self.selector_display[key][0].winfo_children()[1].get() == 1:
+				selected.append(key)
+
+		return selected
 	
 	def delete_file(self,f):
-		self.currently_displayed.pop(f)
-		self.plot(False)
+		for s in self.get_selected():
+			try:
+				self.currently_displayed.pop(s)
+				self.selector_display[s][0].pack_forget()
+				self.selector_display[s][0].destroy()
+				self.selector_display.pop(s)	
+
+			except KeyError:
+				print("\a")
+		self.plot()
+
+	def box_average(self):
+		try:
+			fname = self.file_selector.get()
+			data = self.data_analyzer.box_average(self.currently_displayed[fname])
+			self.currently_displayed.update({fname + "box_av" : data})
+		except KeyError:
+			print("\a")
+		self.plot()
 
 	def update(self):
 		self.file_selector.pack_forget()
@@ -132,23 +190,45 @@ class App(ctk.CTk):
 
 	def incr(self,n):
 		self.fit_bound[0].set(self.fit_bound[0].get()+n)		
-		self.plot(False)
+		self.plot()
 
 	def minu(self,n):
 		self.fit_bound[0].set(self.fit_bound[0].get()-n)
-		self.plot(False)
+		self.plot()
 	
 	def vm_toggle(self):	
 		if self.view_mode == 0:
 			self.view_mode = 1
 		elif self.view_mode == 1:
 			self.view_mode = 0
+		self.plot()
 	
 	def add_new_graph(self):
 		f = self.file_addition_selector.get()
 		[x,y] = self.get_data(f)
 		self.currently_displayed.update({f: [x,y]})
-		self.plot(False)
+	
+		file_frame = ctk.CTkFrame(master = self.selector_frame)
+
+		cb_value = tk.IntVar()
+		label = ctk.CTkLabel(master = file_frame, text = f)
+		cb = ctk.CTkCheckBox(master = file_frame, text = "",variable = cb_value)
+		
+		self.selector_display.update({f: [file_frame,cb_value]})
+
+		self.plot()
+		file_frame.pack()
+		label.grid(row=0, column=0)
+		cb.grid(row=0, column=1)	
+
+	def derivative(self):
+		try:		
+			fname = self.file_selector.get()
+			data = self.data_analyzer.derivative(self.currently_displayed[fname],1)
+			self.currently_displayed.update({fname + "_der" : data})
+		except KeyError:
+			print("\a")
+		self.plot()
 
 	def get_data(self, fname):
 		f = open(sys.argv[1] + "/" + fname, "r")
@@ -163,17 +243,19 @@ class App(ctk.CTk):
 		
 		return x,y	
 	
-	def plot(self,viewmode_toggle):
-		# Switch the viewmode between point-by-point and connected
-		if viewmode_toggle:
-			self.vm_toggle()		
-	
+	def plot(self):
 		# Remake the graph frame. Probably a better way than doing this.	
 		self.graph_frame = ctk.CTkFrame(width = 500, height = 500)
 
 		# The figure that will contain the plot and adding the plot
 		fig = Figure(figsize = (10, 10), dpi = 100)
 		plot1 = fig.add_subplot(111)
+
+		if self.lin_log == 1:
+			plot1.set_yscale("log")			
+		else:
+			plot1.set_yscale("linear")
+		self.update()
 
 		for data in self.currently_displayed.values():
 			# Take the data from the file given	
@@ -184,18 +266,15 @@ class App(ctk.CTk):
 			else:
 				plot1.plot(x,y)
 			
-			[xder,yder] = self.data_analyzer.derivative([x,y],1)
-			plot1.plot(xder,yder)
-		
 			plot1.plot([x[self.fit_bound[0].get()]], [y[self.fit_bound[0].get()]], marker="o", markersize=20, markeredgecolor="red") 
 		
 		self.canvas = FigureCanvasTkAgg(fig, master = self.graph_frame)
 		self.canvas.draw()
 		self.canvas.get_tk_widget().pack()
 		toolbar = NavigationToolbar2Tk(self.canvas, self.graph_frame)
-		plot1.legend(self.currently_displayed.keys())
+		if self.legend_visibility:
+			plot1.legend(self.currently_displayed.keys())
 		self.canvas.get_tk_widget().pack()
-		self.update()
 
 		self.graph_frame.grid(row=0, column = 0)
 	
