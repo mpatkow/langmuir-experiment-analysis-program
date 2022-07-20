fc72ff6b7176c458895588fcec611fb1b46a6548
Theallpro1 <38364214+Theallpro1@users.noreply.github.com>
2022-07-20 15:39:37 -0500

    Update test.py

--- test.py
+++ test.py
@@ -5,6 +5,10 @@ rm = pyvisa.ResourceManager()
 my_instrument = rm.open_resource('GPIB0::14::INSTR')
 
 my_instrument.baud_rate = 19200
+my_instrument.timeout = 25000
+my_instrument.chunk_size = 102400
+my_instrument.open()
+del my_instrument.timeout
 
 my_instrument.write('*IDN?')
 while True:
