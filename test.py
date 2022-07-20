1db6f6ec17685dc63fc789d1d74238568d7eb810
Theallpro1 <38364214+Theallpro1@users.noreply.github.com>
2022-07-20 15:38:20 -0500

    Create test.py

---
+++ test.py
@@ -0,0 +1,12 @@
+
+import pyvisa
+rm = pyvisa.ResourceManager()
+
+my_instrument = rm.open_resource('GPIB0::14::INSTR')
+print(my_instrument.query('*IDN?'))
+
+my_instrument.baud_rate = 19200
+
+my_instrument.write('*IDN?')
+while True:
+    print(my_instrument.read_bytes(1))
